ed66e9ab4729cb1b1ac78e1abe1404ba727e516f
Ahmet Buğra Ertürk <67958913+bugraertrk@users.noreply.github.com>
2022-08-12 15:52:42 +0300

    loginscreen

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import Input from './components/Input/Input';
+import { useState } from 'react';
+import Button from './components/Button/Button';
+import Form from './components/Form/Form';
+function App() {
+  const [userForm, setUserForm]=useState({
+    userName:"",
+    password:""
+  })
+
+  const onValidation = () =>{
+    if(userForm.userName == "" || userForm.password == ""){
+      alert("Geçersiz Form")
+    }else{
+      alert("Form Gönderildi")
+    }
+  }
+  return (
+    <div className='custom-page'> 
+      <Form>
+      <h2>Giriş Sayfası</h2>
+
+      
+    <Input value={userForm.userName} placeholder={"Kullanıcı Adı"} onChange={(e)=>{
+      //setUserForm({userName:e.target.value , password:userForm.password})
+      setUserForm({...userForm, userName:e.target.value})
+    }} />
+
+     <Input type="password" value={userForm.password} placeholder={"Şifre"} onChange={(e)=>{
+      //setUserForm({password:e.target.value , userName:userForm.userName})
+      setUserForm({...userForm, password:e.target.value})
+    }} />
+
+
+
+    <Button text="Giriş Yap" onClick={onValidation} />
+
+    </Form>
+    </div>
+
+  );
+}
+
+export default App;
